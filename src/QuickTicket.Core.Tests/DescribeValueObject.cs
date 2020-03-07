cc87071e433c80dfb6cd23a019f6ec3a78e02705
Ross Jones <ross@appworks.co.za>
2020-03-07 10:27:16 +0200

    Add value object implementation

---
+++ src/QuickTicket.Core.Tests/DescribeValueObject.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace QuickTicket.Core.Tests
+{
+    public class DescribeValueObject
+    {
+        public class DescribeEquals
+        {
+            [Theory]
+            [MemberData(nameof(TestData))]
+            public void WhenComparingUsingOperator_ItShouldCompareQuality(TestValueObject a,
+                TestValueObject b,
+                bool expected)
+            {
+                // Act
+                var actual = a == b;
+                
+                // Assert
+                Assert.Equal(expected, actual);
+            }
+
+            [Theory]
+            [MemberData(nameof(TestData))]
+            public void WhenComparingUsingEqualsMethod_ItShouldCompareEquality(TestValueObject a,
+                TestValueObject b,
+                bool expected)
+            {
+                // Act
+                var actual = a.Equals(b);
+                
+                // Assert
+                Assert.Equal(expected, actual);
+            }
+
+            public static IEnumerable<object[]> TestData()
+            {
+                {
+                    /* Objects with the same properties should be equal */
+                    yield return new object[]
+                    {
+                        new TestValueObject
+                        {
+                            Property = "A"
+                        },
+                        new TestValueObject
+                        {
+                            Property = "A"
+                        },
+                        true
+                    };
+                }
+
+                { 
+                    /* Objects with different properties should not be equal */
+                    yield return new object[]
+                    {
+                        new TestValueObject
+                        {
+                            Property = "A"
+                        },
+                        new TestValueObject
+                        {
+                            Property = "B"
+                        },
+                        false
+                    };
+                }
+
+                { 
+                    /* The same instance of the object should be equal */
+                    var instance = new TestValueObject
+                    {
+                        Property = "A"
+                    };
+                    
+                    yield return new object[]
+                    {
+                        instance,
+                        instance,
+                        true
+                    };
+                }
+            }
+        }
+
+        public class TestValueObject : ValueObject
+        {
+            public string Property { get; set; }
+            
+            protected override IEnumerable<object> GetValuesForEqualityCheck()
+            {
+                yield return Property;
+            }
+        }
+    }
+}
---
+++ src/QuickTicket.Core/ValueObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickTicket.Core
+{
+    /// <summary>
+    /// Sources: https://enterprisecraftsmanship.com/posts/value-object-better-implementation/
+    /// </summary>
+    public abstract class ValueObject
+    {
+        protected abstract IEnumerable<object> GetValuesForEqualityCheck();
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var valueObject = (ValueObject)obj;
+
+            return GetValuesForEqualityCheck().SequenceEqual(valueObject.GetValuesForEqualityCheck());
+        }
+
+        public override int GetHashCode()
+        {
+            var hashcode = new HashCode();
+
+            foreach (var value in GetValuesForEqualityCheck())
+            {
+                hashcode.Add(value);
+            }
+
+            return hashcode.ToHashCode();
+        }
+        
+        public static bool operator ==(ValueObject a, ValueObject b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject a, ValueObject b)
+        {
+            return !(a == b);
+        }
+    }
+}
